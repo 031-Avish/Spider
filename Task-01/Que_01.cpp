a03138af5a2453549dd7c5b2777a3df877fc8a6c
031-Avish <93810970+031-Avish@users.noreply.github.com>
2022-01-18 12:48:36 +0530

    Update Que_01.cpp

--- Task-01/Que_01.cpp
+++ Task-01/Que_01.cpp
@@ -1,1 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+int main() 
+{
+    int  n,m,sum=0,i,j,k,temp;
+    cin>>n>>m;
+    int a[n];
+    for(i=0;i<n;i++) // taking input 
+        cin>>a[i];
+  //Now Sorting the input in accending order 
+     for(i=0;i<n;i++)
+     {
+          for(j=k=i; j<n ;j++)
+         {
+               if(a[j]<a[k])
+               {
+                   k=j;
+               }
+         }
+        temp=a[i];
+        a[i]=a[k];
+        a[k]=temp;
+     }
+  // condition if he can't buy any toffee 
+    if(a[0]>m)
+        cout<<0;
+  // conditon to buy maximum number of toffee 
+    else
+    {
+        i=0;
+        while(sum<=m && i<=n)
+        {
+            sum=sum+a[i];
+            i++;
+        }
+        cout<<i-1;
+    }
+    return 0;
+}
